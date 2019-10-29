8b8881824914e65c884118a6a86e4b8c9e071ebe
YURIc5 <arewane.c5@gmail.com>
2019-10-29 18:25:46 +0900

    google認証追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
         has_many :favorites
         has_many :animals, through: :favorites
         has_one :profile
+
+  def self.find_for_oauth(auth)
+    #登録済みのUserか確認
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    #登録済みのUserではなかった場合
+    unless user.present?
+      password =  Devise.friendly_token[0,20]
+      user = User.new(
+        nickname: auth.info.name,
+        email: auth.info.email,
+        uid: auth.uid,
+        provider: auth.provider,
+      )
+    end
+    return user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers:{
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root to: 'animals#index'
   resources :users, only: [:new, :show]
