abc510bb2adf7f28f3e199ec007596cd8f42759d
YURIc5 <arewane.c5@gmail.com>
2019-11-06 10:16:50 +0900

    search-edit

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,20 +1,23 @@
 class AnimalsController < ApplicationController
+  # before_action :set_search, only: [:show, :index, :seach]
+
   def index
     @animals = Animal.order("created_at DESC")
-    @q = Animal.ransack(params[:q])
-    @search_animals = @q.result(distinct: true)
-    @types = Type.all
+    # @q = Animal.ransack(params[:q])
+    # @search_animals = @q.result(distinct: true)
+    # @types = Type.all
   end
 
   def search
-    @types = Type.all
-    @q = Animal.search(search_params)
-    @animals = @q.result(distinct: true)
+    # @types = Type.all
+    # @q = Animal.search(search_params)
+    # @search_animals = @q.result(distinct: true)
   end
 
   def show
     @animal = Animal.find(params[:id])
   end
+
 
   private
   def search_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_search
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+
+    def set_search
+      @q = Animal.ransack(params[:q])
+      @search_animals = @q.result(distinct: true)
+      @search_types = Type.all
     end
 end
