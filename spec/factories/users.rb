08457ed9464dbb206caa6cff18f54016e1e3d37e
YURIc5 <arewane.c5@gmail.com>
2019-11-07 18:15:17 +0900

    ユーザーログイン単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"yuri"}
+    email                 {"yyy@gmail.com"}
+    password              {"1234567"}
+    password_confirmation {"1234567"}
+  end
+
+end
